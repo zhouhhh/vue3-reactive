3d23a79a0577a94c1868b5ebf3abd709b4566ca7
周行 <793745175@qq.com>
2022-10-04 16:18:28 +0800

    first commit

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,78 @@
+import {extend} from '../shared'
+class ReactiveEffect{
+    private _fn:any
+    public scheduler:Function|undefined
+    deps=[]
+    active=true
+    onStop?:()=>void
+    constructor(fn,scheduler?:Function){
+        this._fn=fn
+        this.scheduler=scheduler
+    }
+    run(){
+        activeEffect=this
+        return this._fn()
+    }
+    stop(){
+        if(this.active){
+            cleanupEffect(this)
+            if(this.onStop){
+                this.onStop()
+            }
+            this.active=false
+        }
+    }
+}
+function cleanupEffect(effect){
+    effect.deps.forEach((dep:any)=>{
+        dep.delete(effect)
+    })
+}
+
+const targetMap=new Map()
+export function track(target,key){
+    // target-->key-->dep
+    let depsMap=targetMap.get(target)
+    if(!depsMap){
+        depsMap=new Map()
+        targetMap.set(target,depsMap)
+    }
+
+    let dep=depsMap.get(key)
+    if(!dep){
+        dep=new Set()
+        depsMap.set(key,dep)
+    }
+    if(!activeEffect) return
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep)
+}
+
+export function trigger(target,key){
+    let depsMap=targetMap.get(target)
+    let dep=depsMap.get(key)
+    for(const effect of dep){
+        if(effect.scheduler){
+            effect.scheduler()
+        }else{
+            effect.run()
+        }
+    }
+}
+
+let activeEffect
+export function effect(fn,options:any={}){
+    const _effect=new ReactiveEffect(fn,options.scheduler)
+    //options
+    //extend
+    extend(_effect,options)
+
+    _effect.run()
+    const runner:any=_effect.run.bind(_effect)
+    runner.effect=_effect
+    return runner
+}
+
+export function stop(runner){
+    runner.effect.stop()
+}
